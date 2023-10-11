7fb82b46d28d113be523e50b6fc73303294eaac6
Jon Brule <jonbrule@gmail.com>
2023-10-11 19:41:12 -0400

    add temp data file

---
+++ web/src/tempData.tsx
@@ -0,0 +1,357 @@
+export const tempList: MovieList[] = [
+	{
+		id: 0,
+		name: 'Movie List 0',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 1,
+				title: 'Movie 2',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 2,
+				title: 'Movie 3',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+	{
+		id: 1,
+		name: 'Movie List 1',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+	{
+		id: 2,
+		name: 'Movie List 2',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 1,
+				title: 'Movie 2',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 2,
+				title: 'Movie 3',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 3,
+				title: 'Movie 4',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 4,
+				title: 'Movie 5',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 5,
+				title: 'Movie 6',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 6,
+				title: 'Movie 7',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+	{
+		id: 3,
+		name: 'Movie List 3',
+		movies: [
+			{
+				id: 1,
+				title: 'Beetlejuice',
+				year: 1988,
+				genre: ['Comedy', 'Fantasy'],
+
+				description:
+					'A couple of recently deceased ghosts contract the services of a "bio-exorcist" in order to remove the obnoxious new owners of their house.',
+			},
+			{
+				id: 2,
+				title: 'The Cotton Club',
+				year: 1984,
+				genre: ['Crime', 'Drama', 'Music'],
+
+				description:
+					'The Cotton Club was a famous night club in Harlem. The story follows the people that visited the club, those that ran it, and is peppered with the Jazz music that made it so famous.',
+			},
+			{
+				id: 3,
+				title: 'The Shawshank Redemption',
+				year: 1994,
+				genre: ['Crime', 'Drama'],
+
+				description:
+					'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+			},
+			{
+				id: 4,
+				title: 'Crocodile Dundee',
+				year: 1986,
+				genre: ['Adventure', 'Comedy'],
+
+				description:
+					'An American reporter goes to the Australian outback to meet an eccentric crocodile poacher and invites him to New York City.',
+			},
+			{
+				id: 5,
+				title: 'Valkyrie',
+				year: 2008,
+				genre: ['Drama', 'History', 'Thriller'],
+
+				description:
+					'A dramatization of the 20 July assassination and political coup description by desperate renegade German Army officers against Hitler during World War II.',
+			},
+			{
+				id: 6,
+				title: 'Ratatouille',
+				year: 2007,
+				genre: ['Animation', 'Comedy', 'Family'],
+
+				description:
+					'A rat who can cook makes an unusual alliance with a young kitchen worker at a famous restaurant.',
+			},
+			{
+				id: 7,
+				title: 'City of God',
+				year: 2002,
+				genre: ['Crime', 'Drama'],
+
+				description:
+					'Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer.',
+			},
+			{
+				id: 8,
+				title: 'Memento',
+				year: 2000,
+				genre: ['Mystery', 'Thriller'],
+
+				description:
+					"A man juggles searching for his wife's murderer and keeping his short-term memory loss from being an obstacle.",
+			},
+			{
+				id: 9,
+				title: 'The Intouchables',
+				year: 2011,
+				genre: ['Biography', 'Comedy', 'Drama'],
+
+				description:
+					'After he becomes a quadriplegic from a paragliding accident, an aristocrat hires a young man from the projects to be his caregiver.',
+			},
+			{
+				id: 10,
+				title: 'Stardust',
+				year: 2007,
+				genre: ['Adventure', 'Family', 'Fantasy'],
+
+				description:
+					"In a countryside town bordering on a magical land, a young man makes a promise to his beloved that he'll retrieve a fallen star by venturing into the magical realm.",
+			},
+			{
+				id: 11,
+				title: 'Apocalypto',
+				year: 2006,
+				genre: ['Action', 'Adventure', 'Drama'],
+
+				description:
+					'As the Mayan kingdom faces its decline, the rulers insist the key to prosperity is to build more temples and offer human sacrifices. Jaguar Paw, a young man captured for sacrifice, flees to avoid his fate.',
+			},
+			{
+				id: 12,
+				title: 'Taxi Driver',
+				year: 1976,
+				genre: ['Crime', 'Drama'],
+
+				description:
+					'A mentally unstable Vietnam War veteran works as a night-time taxi driver in New York City where the perceived decadence and sleaze feeds his urge for violent action, attempting to save a preadolescent prostitute in the process.',
+			},
+			{
+				id: 13,
+				title: 'No Country for Old Men',
+				year: 2007,
+				genre: ['Crime', 'Drama', 'Thriller'],
+
+				description:
+					'Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.',
+			},
+			{
+				id: 14,
+				title: 'Planet 51',
+				year: 2009,
+				genre: ['Animation', 'Adventure', 'Comedy'],
+
+				description:
+					'An alien civilization is invaded by Astronaut Chuck Baker, who believes that the planet was uninhabited. Wanted by the military, Baker must get back to his ship before it goes into orbit without him.',
+			},
+			{
+				id: 15,
+				title: 'Looper',
+				year: 2012,
+				genre: ['Action', 'Crime', 'Drama'],
+
+				description:
+					"In 2074, when the mob wants to get rid of someone, the target is sent into the past, where a hired gun awaits - someone like Joe - who one day learns the mob wants to 'close the loop' by sending back Joe's future self for assassination.",
+			},
+			{
+				id: 16,
+				title: 'Corpse Bride',
+				year: 2005,
+				genre: ['Animation', 'Drama', 'Family'],
+
+				description:
+					'When a shy groom practices his wedding vows in the inadvertent presence of a deceased young woman, she rises from the grave assuming he has married her.',
+			},
+			{
+				id: 17,
+				title: 'The Third Man',
+				year: 1949,
+				genre: ['Film-Noir', 'Mystery', 'Thriller'],
+
+				description:
+					'Pulp novelist Holly Martins travels to shadowy, postwar Vienna, only to find himself investigating the mysterious death of an old friend, Harry Lime.',
+			},
+			{
+				id: 18,
+				title: 'The Beach',
+				year: 2000,
+				genre: ['Adventure', 'Drama', 'Romance'],
+
+				description:
+					'Twenty-something Richard travels to Thailand and finds himself in possession of a strange map. Rumours state that it leads to a solitary beach paradise, a tropical bliss - excited and intrigued, he sets out to find it.',
+			},
+			{
+				id: 19,
+				title: 'Scarface',
+				year: 1983,
+				genre: ['Crime', 'Drama'],
+
+				description:
+					'In Miami in 1980, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.',
+			},
+			{
+				id: 20,
+				title: 'Sid and Nancy',
+				year: 1986,
+				genre: ['Biography', 'Drama', 'Music'],
+
+				description:
+					'Morbid biographical story of Sid Vicious, bassist with British punk group the Sex Pistols, and his girlfriend Nancy Spungen. When the Sex Pistols break up after their fateful US tour, ...',
+			},
+		],
+	},
+	{
+		id: 4,
+		name: 'Movie List 4',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 1,
+				title: 'Movie 2',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+	{
+		id: 5,
+		name: 'Movie List 5',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 1,
+				title: 'Movie 2',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+	{
+		id: 6,
+		name: 'Movie List 6',
+		movies: [
+			{
+				id: 0,
+				title: 'Movie 1',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 1,
+				title: 'Movie 2',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 2,
+				title: 'Movie 3',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+			{
+				id: 3,
+				title: 'Movie 4',
+				description: 'description of movie',
+				genre: ['Comedy', 'Action', 'Drama'],
+				year: 2023,
+			},
+		],
+	},
+];
