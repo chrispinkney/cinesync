64d056ac3f35e485458d189f748f26f4a81e01a6
Jon Brule <jonbrule@gmail.com>
2023-10-15 15:22:58 -0400

    add temporary Login button

---
+++ web/src/components/Login/Login.tsx
@@ -0,0 +1,78 @@
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar';
+import { Fragment, useState } from 'react';
+
+// Temporary login to get cookies for list fetching
+const Login = () => {
+	const [loggingIn, setLoggingIn] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [snackbarOpen, setSnackbarOpen] = useState(false);
+	const [errorText, setErrorText] = useState('');
+
+	const handleClose = (
+		_event: React.SyntheticEvent | Event,
+		reason?: string,
+	) => {
+		if (reason === 'clickaway') {
+			return;
+		}
+		setSnackbarOpen(false);
+	};
+
+	return (
+		<Fragment>
+			<Button
+				variant="contained"
+				disabled={loggingIn}
+				onClick={async () => {
+					setLoggingIn(true);
+					const response = await fetch(
+						`${process.env.NEXT_PUBLIC_URL}/auth/signin`,
+						{
+							method: 'POST',
+							headers: {
+								Accept: 'application/json',
+								'Content-Type': 'application/json',
+							},
+							credentials: 'include',
+							body: JSON.stringify({
+								email: process.env.NEXT_PUBLIC_LOGIN_EMAIL,
+								password: process.env.NEXT_PUBLIC_LOGIN_PASSWORD,
+							}),
+						},
+					);
+					if (response.ok) {
+						setSuccess(true);
+					} else {
+						setSuccess(false);
+						setErrorText(`${response.status} - ${response.statusText}`);
+					}
+					setSnackbarOpen(true);
+					setLoggingIn(false);
+				}}
+			>
+				Login
+			</Button>
+			{loggingIn && <CircularProgress />}
+			<Snackbar
+				open={snackbarOpen}
+				autoHideDuration={3000}
+				onClose={handleClose}
+			>
+				<Alert
+					onClose={handleClose}
+					severity={success ? 'success' : 'error'}
+					sx={{ width: '100%' }}
+				>
+					{success
+						? 'Logged in successfully'
+						: `Login attempt failed: ${errorText}`}
+				</Alert>
+			</Snackbar>
+		</Fragment>
+	);
+};
+
+export default Login;
--- web/src/context/lists.context.tsx
+++ web/src/context/lists.context.tsx
@@ -28,6 +28,7 @@ export const ListsContextProvider = ({ children }: { children: ReactNode }) => {
 		});
 		const fetchedLists = (await response.json()).List;
 		setLists(fetchedLists);
+		console.dir(fetchedLists);
 	};
 
 	// Fetch once upon initial context load
