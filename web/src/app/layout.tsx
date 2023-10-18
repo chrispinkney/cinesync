93de0532ea1b1dde26c93b21db46774f46a5df6e
Chris Pinkney <hey@chrispinkney.com>
2023-10-17 21:34:51 -0400

    chore(web): fix Context case, dockerfile

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,33 @@
-'use client';
 import { ReactNode } from 'react';
 import ThemeRegistry from '../../theme/ThemeRegistry';
 import { GlobalContextProvider } from '@/context/store';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Cinesync',
+		default: 'Cinesync - Synchronized Film Watchlists',
+	},
+	description: 'Create and share movie lists with friends.',
+	openGraph: {
+		title: {
+			template: '%s | Cinesync',
+			default: 'Cinesync - Synchronized Film Watchlists',
+		},
+		description: 'Create and share movie lists with friends.',
+		url: 'https://cinemasync.me/',
+		type: 'website',
+		images: [
+			{
+				url: '/cinesync-icon.png',
+				width: 160,
+				height: 168,
+				alt: 'Cinesync logo',
+			},
+		],
+	},
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
 	return (
